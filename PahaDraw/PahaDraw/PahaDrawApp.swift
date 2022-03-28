69411dffe164440fd519477e4654cc9c30e2448f
Nguyễn Develop <nguyendevelop@hotmail.com>
2022-03-28 15:48:09 +0700

    create project

---
+++ PahaDraw/PahaDraw/PahaDrawApp.swift
@@ -0,0 +1,17 @@
+//
+//  PahaDrawApp.swift
+//  PahaDraw
+//
+//  Created by Nguyen Dang Quy on 28/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct PahaDrawApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
